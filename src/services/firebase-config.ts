031fea1433e1c2877550329b587b875d8bc920c9
Sagi Bachar Adler <sagienator@gmail.com>
2024-01-28 15:21:46 +0200

    2024_01_28

--- src/services/firebase-config.ts
+++ src/services/firebase-config.ts
@@ -4,12 +4,12 @@ import { getStorage } from 'firebase/storage';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
+  apiKey: "AIzaSyDyldBE7qw-QQjdyhzjsrryNbKUU4RfGyc",
+  authDomain: "marketplace-e6f3f.firebaseapp.com",
+  projectId: "marketplace-e6f3f",
+  storageBucket: "marketplace-e6f3f.appspot.com",
+  messagingSenderId: "716969957847",
+  appId: "1:716969957847:web:8b30a82ffa8263a1fb1392"
 };
 
 const app = initializeApp(firebaseConfig);
